d40e7cb056a30831ede3b2ebff990b197a0e0079
Jasper-Rowan <jasper.rowan@berkeley.edu>
2022-07-11 14:55:07 -0700

    Can now select number of rows and cols, using for loops to create arrays and then calling map in a nested jsx expression, this may not be the best way to do this but it will work for now and is something to come back to.

--- src/components/PathfindingVisualizer.jsx
+++ src/components/PathfindingVisualizer.jsx
@@ -1,20 +1,34 @@
 import Node from "./Node";
 import "./PathfindingVisualizer.css"
 
-const GRID_ROWS = 10
-const GRID_COLS = 10
+const GRID_ROWS = 20
+const GRID_COLS = 20
 
 
 const PathfindingVisualizer = () => {
   // const [grid, setGrid] = useState([]);
-  const grid = [];
-  for (let i = 0; i < GRID_ROWS; i++)
-    for (let j = 0; j < GRID_COLS; j++) 
-        grid.push([i, j]);
+  const gridRows = [];
+  const gridCols = [];
+  for (let i = 0; i < GRID_ROWS; i++) 
+    gridRows.push(i)
+  for (let j = 0; j < GRID_COLS; j++) 
+      gridCols.push(j);
+  
+    
 
   return (
     <div className="gridContainer">
-     {grid.map((i) => <Node></Node>)} 
+    {gridRows.map((i) => {
+    return (
+      <div style={{ display: "flex", width: "100%" }}>
+        {gridCols.map((i) => (
+          <Node></Node>
+        ))}
+      </div>
+    );
+    } )}
+    
+    
     </div>
   );
 }
